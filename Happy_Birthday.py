0cd4ca41728b0bdc058127a51c0e4793b374eebd
Snoutband <76590025+Snoutband@users.noreply.github.com>
2022-01-19 07:56:28 -0600

    Update Happy_Birthday.py

--- Happy_Birthday.py
+++ Happy_Birthday.py
@@ -1,17 +1,20 @@
 from datetime import datetime
 datemonth = datetime.today().month
 dateday = datetime.today().day
-decision = input("Check to see, Yes or No ")
+decision = input("Check?, Yes or No ")
 if decision == "Yes":
-    if datemonth == 1 and dateday == 26:
-        print("It is your birthday!")
-        for x in range("Happy Birthday!"):
-            print(x)
-    elif datemonth == 1:
-        print("Birthday Month!")
-    elif datemonth == 12:
-        print("One Month away!")
+    I = input("Enter your intitals ")
+    Ilist = ['TS']
+    if I in Ilist:
+        if datemonth == 1 and dateday == 26:
+            print("It is your birthday!")
+            for x in range("Happy Birthday!"):
+                print(x)
+        elif datemonth == 1:
+                print("Birthday Month!")
+        elif datemonth == 12:
+                print("One Month away!")
+        else:
+            print("Better luck next time")
     else:
-        print("Better luck next time")
-else:
-    print("")
+        print("")
